204971d290002dff232f224294cc561c8b90d258
camilo <camiloacevedomoreno@gmail.com>
2023-03-26 00:22:35 -0500

    aside game card

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,24 @@
-import React from "react";
-import '../styles/Sidebar.css'
+import React from "react"
+import "../styles/Sidebar.css"
 
-export default function Sidebar() {
-
-    return (
-        <aside>
-            <div className="aside-content">
-                
-            </div>
-        </aside>
-    )
+export default function Sidebar(props) {
+	const items = props.games.map((game, index) => {
+        return (
+            <div className="game-item">
+                        <p>#: {index+1}</p>
+                        <p>Time: {game.info.timeStamp}s</p>
+                    </div>
+        )
+    })
+	return (
+		<aside>
+			<div className="aside-content">
+				<h2 className="sidebar-title">Games</h2>
+				<div className="game-items">
+					{items}
+				</div>
+			</div>
+		</aside>
+	)
 }
+
